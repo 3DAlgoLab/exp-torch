af2e5acef95208c476a035422c4943b2530f871d
3DAlgoLab <83936830+3DAlgoLab@users.noreply.github.com>
2022-03-07 11:20:58 +0900

    torch graph

--- agraph.py
+++ agraph.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     print('Start!')
     print(torch.__version__)
     print('Scalar inputs:', compute_z(torch.tensor(1),
-                                      torch.tensor(2), torch.tensor(3)))
+                                      torch.tensor(2),
+                                      torch.tensor(3)))
